c11ee25668cb8f2106688ced9a840f0dbeaaf34e
Dima <v3il@ukr.net>
2019-11-02 23:43:10 +0200

    Values editor for RadioButtonField

--- client/src/components/fields/radiobutton_field/RadioButtonFieldValues.vue
+++ client/src/components/fields/radiobutton_field/RadioButtonFieldValues.vue
@@ -1,15 +1,82 @@
 <template>
-    <div>
-        RadioFieldValues
+    <div class="checkbox-values">
+        <h3 class="checkbox-values__field-title">{{ field.params.label }}</h3>
+
+        <div class="checkbox-values__values-editor" v-for="option in field.params.options">
+            <label class="checkbox-values__value-block">
+                <span class="checkbox-values__value-label">Значение включённого состояния</span>
+
+                <input
+                        type="text"
+                        class="checkbox-values__value-input text-input"
+                        v-model.number="option.activatedValue"
+                >
+            </label>
+
+            <label class="checkbox-values__value-block">
+                <span class="checkbox-values__value-label">Значение выключенного состояния</span>
+
+                <input
+                        type="text"
+                        class="checkbox-values__value-input text-input"
+                        v-model.number="option.deactivatedValue"
+                >
+            </label>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "RadioButtonValues"
-    }
+        name: 'RadioFieldValues',
+
+        props: {
+            field: {
+                type: Object,
+                required: true,
+            }
+        },
+
+        methods: {
+            refreshValue() {
+                this.field.params.value = this.field.params.selectedOption.activatedValue;
+            }
+        },
+
+        watch: {
+            'field.params.options': {
+                deep: true,
+
+                handler() {
+                    this.refreshValue();
+                }
+            },
+        }
+    };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .checkbox-values {
+        max-width: 1600px;
+        margin: 0 auto;
 
+        &__values-editor {
+            display: flex;
+            align-items: center;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            margin: 0 -12px;
+        }
+
+        &__value-block {
+            min-width: 300px;
+            margin: 6px 12px;
+            flex: 1;
+        }
+
+        &__value-label {
+            display: block;
+            margin-bottom: 6px;
+        }
+    }
 </style>
