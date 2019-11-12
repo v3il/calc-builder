de783083c65009b7d6998e82c044c5722c92a0c2
Dima <v3il@ukr.net>
2019-11-12 18:22:43 +0200

    Configured default styles for new slider

--- client/src/main.js
+++ client/src/main.js
@@ -1,18 +1,8 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
-import KeenUI from 'keen-ui';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
-
-import 'keen-ui/dist/keen-ui.css';
-
-import 'vue-slider-component/theme/default.css';
-
-Vue.use(KeenUI);
-Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
