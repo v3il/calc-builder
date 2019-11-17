0ae97db2864cae2fc4a62c41924ab9c3b07efaab
Dima <v3il@ukr.net>
2019-11-17 19:43:05 +0200

    Custom UI element: Text

--- client/src/components/field_properties_editors/TextFieldComponent.vue
+++ client/src/components/field_properties_editors/TextFieldComponent.vue
@@ -4,26 +4,14 @@
             {{ options.title }}
         </h4>
 
-        <input-element :value="value" @input="$emit('input', $event)" :options="options" />
-
-<!--        <input-->
-<!--            :type="options.type || 'text'"-->
-<!--            class="text-field__input text-input"-->
-<!--            @input="emitUpdate"-->
-<!--            @blur="updateCurrentValue"-->
-<!--            :value="value"-->
-<!--            :placeholder="options.placeholder"-->
-<!--            :id="options.id"-->
-<!--            :style="options.style"-->
-<!--            ref="input"-->
-<!--        />-->
+        <input-element :value="value" @input="$emit('input', $event)" :options="options" class="text-field__input" />
 
         <p class="text-field__description" v-if="options.description">{{ options.description }}</p>
     </div>
 </template>
 
 <script>
-    import InputElement from '@/components/ui_elements/InputElement'
+import InputElement from '@/components/ui_elements/InputElement';
 
 export default {
     name: 'TextFieldComponent',
@@ -43,56 +31,10 @@ export default {
             default: () => ({}),
         },
     },
-
-    // data() {
-    //     return {
-    //         prevValue: this.value,
-    //         currentValue: this.value,
-    //     };
-    // },
-    //
-    // methods: {
-    //     emitUpdate() {
-    //         const input = this.$refs.input;
-    //
-    //         let value = input.value;
-    //         const { validator, minLength } = this.options;
-    //
-    //         if (minLength && value.length < minLength) {
-    //             value = this.prevValue;
-    //         }
-    //
-    //         if (validator) {
-    //             const validationResult = validator(value, this.prevValue);
-    //
-    //             if (validationResult === false) {
-    //                 value = this.prevValue;
-    //             }
-    //
-    //             if (typeof validationResult !== 'boolean') {
-    //                 value = validationResult;
-    //             }
-    //         }
-    //
-    //         this.$emit('input', value);
-    //
-    //         this.prevValue = value;
-    //         this.currentValue = value;
-    //         input.value = value;
-    //
-    //         this.$emit('value-changed', value);
-    //     },
-    //
-    //     updateCurrentValue() {
-    //         this.$refs.input.value = this.currentValue;
-    //     },
-    // },
 };
 </script>
 
 <style scoped lang="scss">
-@import '../../common-styles/text-input';
-
 .text-field {
     &__input {
         margin: 6px 0;
--- client/src/components/ui_elements/InputElement.vue
+++ client/src/components/ui_elements/InputElement.vue
@@ -15,78 +15,78 @@
 </template>
 
 <script>
-    export default {
-        name: "Input",
+export default {
+    name: 'InputElement',
 
-        props: {
-            value: {
-                type: [String, Number],
-                required: true,
-            },
-
-            options: {
-                type: Object,
-                default: () => ({}),
-            },
+    props: {
+        value: {
+            type: [String, Number],
+            required: true,
         },
 
-        data() {
-            return {
-                prevValue: this.value,
-                currentValue: this.value,
-            };
+        options: {
+            type: Object,
+            default: () => ({}),
         },
+    },
 
-        methods: {
-            emitUpdate() {
-                const input = this.$el;
+    data() {
+        return {
+            prevValue: this.value,
+            currentValue: this.value,
+        };
+    },
 
-                let value = input.value;
-                const { validator, minLength } = this.options;
+    methods: {
+        emitUpdate() {
+            const input = this.$el;
 
-                if (minLength && value.length < minLength) {
+            let value = input.value;
+            const { validator, minLength } = this.options;
+
+            if (minLength && value.length < minLength) {
+                value = this.prevValue;
+            }
+
+            if (validator) {
+                const validationResult = validator(value, this.prevValue);
+
+                if (validationResult === false) {
                     value = this.prevValue;
                 }
 
-                if (validator) {
-                    const validationResult = validator(value, this.prevValue);
-
-                    if (validationResult === false) {
-                        value = this.prevValue;
-                    }
-
-                    if (typeof validationResult !== 'boolean') {
-                        value = validationResult;
-                    }
+                if (typeof validationResult !== 'boolean') {
+                    value = validationResult;
                 }
+            }
 
-                this.prevValue = value;
-                this.currentValue = value;
-                input.value = value;
+            this.prevValue = value;
+            this.currentValue = value;
+            input.value = value;
 
-                this.$emit('input', value);
-            },
-
-            updateCurrentValue() {
-                this.$el.value = this.currentValue;
-            },
+            this.$emit('input', value);
         },
-    }
+
+        updateCurrentValue() {
+            this.$el.value = this.currentValue;
+        },
+    },
+};
 </script>
 
 <style scoped lang="scss">
-    .text-input {
-        padding: 0 1.2rem;
-        font-size: 0.93rem;
-        outline: none;
-        height: 2.4rem;
-        color: $color_primary;
-        border: 2px solid $gray;
-        background-color: $white;
-        border-radius: 0.6rem;
-        position: relative;
-        transition: border 0.3s linear;
-        box-sizing: border-box;
-        width: 100%;
-    }
+.text-input {
+    padding: 0 1.2rem;
+    font-size: 0.93rem;
+    outline: none;
+    height: 2.4rem;
+    color: $color_primary;
+    border: 2px solid $gray;
+    background-color: $white;
+    border-radius: 0.6rem;
+    position: relative;
+    transition: border 0.3s linear;
+    box-sizing: border-box;
+    width: 100%;
+}
 </style>
