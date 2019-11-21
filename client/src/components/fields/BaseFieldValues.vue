faa2add01c45f3df0424923e323755afc3ea250f
Dima <v3il@ukr.net>
2019-11-21 23:56:01 +0200

    Letters render in values components

--- client/src/components/fields/BaseFieldValues.vue
+++ client/src/components/fields/BaseFieldValues.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="base-field-values" :class="`base-field-values--${fieldClass}`">
-        <h3 class="base-field-values__field-title">{{ field.params.label }} ({{ fieldClass }})</h3>
-        <slot></slot>
+        <div class="base-field-values__letter-column">
+            <span class="base-field-values__field-letter">{{ field.letter }}</span>
+        </div>
+
+        <div class="base-field-values__properties-column">
+            <input-element v-model="field.params.label" />
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
+import InputElement from '../ui_elements/InputElement';
+
 export default {
     name: 'BaseFieldValues',
+
+    components: {
+        InputElement,
+    },
 
     props: {
         field: {
@@ -26,17 +38,46 @@ export default {
 .base-field-values {
     padding: 12px 24px;
     border-radius: 6px;
+    display: flex;
+
+    &__letter-column {
+        flex: auto 0;
+        margin-right: 24px;
+    }
+
+    &__properties-column {
+        flex: 1;
+    }
+
+    &__field-letter {
+        padding: 9px 12px;
+        display: inline-block;
+        border-radius: 6px;
+        color: $white;
+    }
 
     &--select {
         background-color: $select_widget_light;
+    }
+
+    &--select &__field-letter {
+        background-color: $select_widget;
     }
 
     &--checkbox {
         background-color: $checkbox_widget_light;
     }
 
+    &--checkbox &__field-letter {
+        background-color: $checkbox_widget;
+    }
+
     &--radio {
         background-color: $radio_widget_light;
+    }
+
+    &--radio &__field-letter {
+        background-color: $radio_widget;
     }
 }
 </style>
