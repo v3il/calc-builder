fae3866cab20bc660eb858acab37388bb714abb6
Dima <v3il@ukr.net>
2019-12-02 18:21:44 +0200

    SelectField value update

--- client/src/components/fields/result_field/ResultField.vue
+++ client/src/components/fields/result_field/ResultField.vue
@@ -35,8 +35,6 @@ export default {
 
             const processedFormula = this.field.params.formula.replace(/[A-Z]\d?/g, fieldLetter => {
                 const field = this.getFieldByLetter(fieldLetter);
-                console.log(field);
-
                 return field && field.params.value ? field.params.value : 0;
             });
 
