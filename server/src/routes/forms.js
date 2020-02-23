2acdc5c4d78b593c2e22cd53eb5886a1bdff5eee
Dima <v3il@ukr.net>
2020-02-23 23:10:01 +0200

    User forms GET handler

--- server/src/routes/forms.js
+++ server/src/routes/forms.js
@@ -1,8 +1,24 @@
+const authGuard = require('../guards');
+
+const knexInstance = require('../knexInstance');
+
 module.exports = app => {
-    app.get('/forms', (request, response) => {
-        response.status(200).json({
-            status: 'Ok',
-        });
+    app.get('/forms', authGuard, async (request, response) => {
+        const user = request.user;
+
+        try {
+            const userForms = await knexInstance('forms')
+                .where({
+                    user_id: user.id,
+                })
+                .select();
+
+            response.status(200).json(userForms);
+        } catch (error) {
+            response.status(500).send({
+                error: error.message,
+            });
+        }
     });
 
     app.post('/forms', (request, response) => {
