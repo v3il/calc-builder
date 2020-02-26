c7a1fafccdc64cadf8c3b8340e9d9fdd02df5549
Dima <v3il@ukr.net>
2020-02-26 10:56:57 +0200

    Registration

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const { promisify } = require('util');
+// const { promisify } = require('util');
 
 const express = require('express');
-const expressSession = require('express-session');
+// const expressSession = require('express-session');
 const initRoutes = require('./routes');
 const initPassport = require('./passport');
 
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 const passport = require('passport');
 
-const knexInstance = require('./knexInstance');
+// const knexInstance = require('./knexInstance');
 
 const app = express();
 
@@ -21,68 +21,68 @@ app.use(cors());
 app.use(require('cookie-parser')());
 app.use(bodyParser.json('combine'));
 
-var KnexSessionStore = require('connect-session-knex')(expressSession);
-var store = new KnexSessionStore({
-    knex: knexInstance,
-});
-
-// app.configure(function() {
-    app.use(
-        expressSession({
-            store: store,
-            secret: 'your secret',
-            resave: true,
-            saveUninitialized: true,
-            cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
-        }),
-    );
+// var KnexSessionStore = require('connect-session-knex')(expressSession);
+// var store = new KnexSessionStore({
+//     knex: knexInstance,
 // });
 
-passport.serializeUser((user, done) => {
-    console.log('S', user);
+// app.configure(function() {
+//     app.use(
+//         expressSession({
+//             store: store,
+//             secret: 'your secret',
+//             resave: true,
+//             saveUninitialized: true,
+//             cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
+//         }),
+//     );
+// });
 
-    done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    console.log('D', id);
-
-    try {
-        done(
-            null,
-            await knexInstance('users')
-                .where({ id })
-                .select(),
-        );
-    } catch (err) {
-        done(err);
-    }
-});
+// passport.serializeUser((user, done) => {
+//     console.log('S', user);
+//
+//     done(null, user.id);
+// });
+//
+// passport.deserializeUser(async (id, done) => {
+//     console.log('D', id);
+//
+//     try {
+//         done(
+//             null,
+//             await knexInstance('users')
+//                 .where({ id })
+//                 .select(),
+//         );
+//     } catch (err) {
+//         done(err);
+//     }
+// });
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req, res, next) => {
-    console.log(req.user)
-
-    // If user is disabled, redirect him to login form
-    if (req.user) {
-        req.session.destroy();
-        req.logout();
-
-        // if (req.method === 'POST') {
-        //     // If it is a POST request (API), send the 301th status
-        //     // to redirect user in JS
-        //     res.sendStatus(301);
-        // } else {
-        //     // If GET request, redirect user on server
-        //     res.redirect('/');
-        // }
-    } else {
-        // req.loginAsync = promisify(req.login);
-        next();
-    }
-});
+// app.use((req, res, next) => {
+//     console.log(req.user)
+//
+//     // If user is disabled, redirect him to login form
+//     if (req.user) {
+//         req.session.destroy();
+//         req.logout();
+//
+//         // if (req.method === 'POST') {
+//         //     // If it is a POST request (API), send the 301th status
+//         //     // to redirect user in JS
+//         //     res.sendStatus(301);
+//         // } else {
+//         //     // If GET request, redirect user on server
+//         //     res.redirect('/');
+//         // }
+//     } else {
+//         // req.loginAsync = promisify(req.login);
+//         next();
+//     }
+// });
 
 initRoutes(app);
 initPassport(app);
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,140 +1,128 @@
-const passport = require('passport');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const knexInstance = require('../knexInstance');
 
 module.exports = app => {
-    app.post('/login', async (request, response) => {
-        const { login, password } = request.body;
-
-        if (!(login && login.length >= 4)) {
-            return response.status(400).json({
-                error: 'Логин не может быть меньше, чем 4 символа',
-            });
-        }
-
-        if (!(password && password.length >= 6)) {
-            return response.status(400).json({
-                error: 'Пароль не может быть меньше, чем 6 символов',
-            });
-        }
-
-        try {
-            const users = await knexInstance('users')
-                .where({ login })
-                .select();
-
-            if (!users.length) {
-                return response.status(400).json({
-                    error: 'Неправильный логин',
-                });
-            }
-
-            const user = users[0];
-
-            const salt = await bcrypt.genSalt(3);
-            const encryptedPassword = await bcrypt.hashSync(password, salt);
-
-            if (user.password !== encryptedPassword) {
-                return response.status(400).json({
-                    error: 'Неправильный пароль',
-                });
-            }
-
-            const userPublicData = { id: user.id, login: user.login };
-
-            const token = jwt.sign(userPublicData, process.env.JWT_SECRET, {
-                expiresIn: process.env.JWT_DURATION,
-            });
-
-            response.status(200).json({
-                token,
-                user: userPublicData,
-            });
-        } catch (error) {
-            console.error(error);
-
-            response.status(500).json({
-                error: error.message,
-            });
-        }
-    });
-
-    app.post('/register', async (request, response) => {
-        const { login, password } = request.body;
-
-        if (!(login && login.length >= 4)) {
-            return response.status(400).json({
-                error: 'Логин не может быть меньше, чем 4 символа',
-            });
-        }
-
-        try {
-            const usersWithSameLogin = await knexInstance('users')
-                .where({ login })
-                .select();
-
-            if (usersWithSameLogin.length) {
-                return response.status(400).json({
-                    error: `Пользователь с логином "${login}" уже существует`,
-                });
-            }
-        } catch (error) {
-            console.error(error);
-
-            response.status(500).json({
-                error: error.message,
-            });
-        }
-
-        if (!(password && password.length >= 4)) {
-            return response.status(400).json({
-                error: 'Пароль не может быть меньше, чем 4 символа',
-            });
-        }
-
-        try {
-            const salt = await bcrypt.genSalt(3);
-            const encryptedPassword = await bcrypt.hashSync(password, salt);
-
-            const newUserIds = await knexInstance('users').insert({
-                login,
-                password: encryptedPassword,
-            });
-
-            const createdUsers = await knexInstance('users')
-                .where({
-                    id: newUserIds[0],
-                })
-                .select();
-
-            console.log(createdUsers[0]);
-
-            // const token = jwt.sign({ id: createdUsers[0].id, login: createdUsers[0].login }, process.env.JWT_SECRET, {
-            //     expiresIn: '7d',
-            // });
-            //
-            // console.log(request.cookies)
-            //
-            // response.cookie('jwt_token', token, {
-            //     httpOnly: false,
-            //     maxAge: 900000,
-            // });
-            //
-            // console.log(token)
-
-            response.status(200).json({
-                // token,
-                created: true,
-                userId: newUserIds[0],
-            });
-        } catch (error) {
-            console.error(error);
-
-            response.status(500).json({
-                error: error.message,
-            });
-        }
-    });
+    app.post('/login', login);
+    app.post('/register', register);
+    app.post('/logout', logout);
 };
+
+async function login(request, response) {
+    const { login, password } = request.body;
+
+    if (!login) {
+        return response.status(400).json({
+            error: 'Неправильный логин',
+        });
+    }
+
+    if (!password) {
+        return response.status(400).json({
+            error: 'Неправильный пароль',
+        });
+    }
+
+    try {
+        const users = await knexInstance('users')
+            .where({ login })
+            .select();
+
+        if (!users.length) {
+            return response.status(400).json({
+                error: 'Неправильный логин',
+            });
+        }
+
+        const user = users[0];
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            return response.status(400).json({
+                error: 'Неправильный пароль',
+            });
+        }
+
+        const userPublicData = { id: user.id, login: user.login };
+
+        const token = jwt.sign(userPublicData, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_DURATION,
+        });
+
+        response.status(200).json({
+            token,
+            user: userPublicData,
+        });
+    } catch (error) {
+        console.error(error);
+
+        response.status(500).json({
+            error: error.message,
+        });
+    }
+}
+
+async function register(request, response) {
+    const { login, password } = request.body;
+
+    if (!(login && login.trim().length > 0)) {
+        return response.status(400).json({
+            error: 'Неправильный логин',
+        });
+    }
+
+    if (!(password && password.trim().length > 4)) {
+        return response.status(400).json({
+            error: 'Неправильный пароль',
+        });
+    }
+
+    try {
+        const usersWithSameLogin = await knexInstance('users')
+            .where({ login })
+            .select();
+
+        if (usersWithSameLogin.length) {
+            return response.status(400).json({
+                error: `Пользователь с логином "${login}" уже существует`,
+            });
+        }
+
+        const salt = await bcrypt.genSalt(3);
+        const encryptedPassword = await bcrypt.hashSync(password, salt);
+
+        const newUserIds = await knexInstance('users').insert({
+            login,
+            password: encryptedPassword,
+        });
+
+        const createdUsers = await knexInstance('users')
+            .where({
+                id: newUserIds[0],
+            })
+            .select();
+
+        const user = createdUsers[0];
+        const userPublicData = { id: user.id, login: user.login };
+
+        const token = jwt.sign(userPublicData, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+        });
+
+        response.status(200).json({
+            token,
+            user: userPublicData,
+        });
+    } catch (error) {
+        console.error(error);
+
+        response.status(500).json({
+            error: error.message,
+        });
+    }
+}
+
+async function logout(request, response) {
+
+}
